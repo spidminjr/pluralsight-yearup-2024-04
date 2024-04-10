dfc13cc331f2e3314029f34e11e76e56e5b80957
venomgto <95735342+venomgto@users.noreply.github.com>
2024-04-10 15:58:43 -0700

    class assignment

--- workbook-1/DeclaringVariables/src/main/java/com/pluralsight/Main.java
+++ workbook-1/DeclaringVariables/src/main/java/com/pluralsight/Main.java
@@ -7,33 +7,35 @@ public class Main
         // declare variables for each of the following:
 
         // a vehicle identification number in the range 1000000 - 9999999
-
+        int vehicleIdentification = 5562395;
 
         // a vehicle make /model (i.e. Ford Explorer)
-
+        String vehicleMake = "Toyota Corolla GR";
 
         // a vehicle color
-
+        String vehicleColor = "Red";
 
         // whether the vehicle has a towing package
-
+        boolean vehicleTowing = false;
 
         // an odometer reading
+        int vehicleMileage = 178345;
 
 
         // a price
-
+        int vehiclePrice = 52567;
 
         // a quality rating (A, B, or C)
-
+        char qualityRating = 'A';
 
         // a phone number
-
+        byte phoneNumber = (byte) 123456789;
 
         // a social security number
-
+        int socialSecurityNumber = 346172345;
 
         // a zip code
+        int zipCode = 94606;
 
 
 
--- workbook-1/demos/03-IntroToJava/src/main/java/com/pluralsight/Main.java
+++ workbook-1/demos/03-IntroToJava/src/main/java/com/pluralsight/Main.java
@@ -4,6 +4,34 @@ public class Main
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello world!");
+        //System.out.println("Hello world!");
+        // variable naming
+        //must start with a letter or _
+        //cannot have spaces in the name
+        //camelCasing
+        // snake_case -- not really used in java
+        //UPPER_CASE -- in java this is for constants
+        // kebab-case -- does not work in java (but used in other languages)
+
+        // declare variable that has the count of kids on a bus
+        byte countOfKidsOnBus;
+        countOfKidsOnBus = 127;
+
+        System.out.println("Kids on bus: " + countOfKidsOnBus);
+        // declare a variable that has acount of the number of the buses in the school district
+        int countOfBusesInDistrict = 12;
+        System.out.println("Buses in the school district: " + countOfBusesInDistrict);
+
+        //price of chocolate
+        double priceOfChocolate = 1.5;
+
+        // are all of the kids on the bus?
+        boolean kidsOnBus = true;
+
+        // the first letter of the alphabet
+        char firstLetter = 'a';
+
+        // name of your favorite restaurant
+        String favoriteRestaurant = "Texas Roadhouse";
     }
 }
