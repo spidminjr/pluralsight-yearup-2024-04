fdfe7e0c7d1725ab4ba764432daf0477abd00aba
venomgto <95735342+venomgto@users.noreply.github.com>
2024-04-16 08:24:18 -0700

    Exercise 2

--- workbook-2/demos/FullNameParser/src/main/java/com/pluralsight/Main.java
+++ workbook-2/demos/FullNameParser/src/main/java/com/pluralsight/Main.java
@@ -1,7 +1,20 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+    {
+
+    }
+
+    public static void fullName()
+    {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter your full name: ");
+        String fullName = scanner.nextLine();
+
+
     }
 }
