1c66e996c0f33e7594a665a54cb76bbb16b41c9f
venomgto <95735342+venomgto@users.noreply.github.com>
2024-04-16 09:26:28 -0700

    Excercise 2 updated

--- workbook-2/demos/FullNameParser/src/main/java/com/pluralsight/Main.java
+++ workbook-2/demos/FullNameParser/src/main/java/com/pluralsight/Main.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args)
     {
-
+        fullName();
     }
 
     public static void fullName()
@@ -15,6 +15,29 @@ public class Main {
         System.out.print("Enter your full name: ");
         String fullName = scanner.nextLine();
 
+        String firstName = "";
+        String middleName = "";
+        String lastName = "";
 
+        String[] nameParts = fullName.split(" ");
+
+        firstName = nameParts[0];
+
+        boolean hasMiddleName = nameParts.length > 2;
+        if (hasMiddleName)
+        {
+            middleName = nameParts[1];
+            lastName = nameParts[2];
+
+        }
+        else
+        {
+            lastName = nameParts[1];
+        }
+
+        System.out.println();
+        System.out.printf("First Name: %s \n", firstName);
+        System.out.printf("Middle Name: %s \n", middleName);
+        System.out.printf("Last Name: %s \n", lastName);
     }
 }
