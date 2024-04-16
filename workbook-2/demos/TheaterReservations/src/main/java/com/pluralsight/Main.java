168472b63cdee89879250ebfaa1fb33572375285
venomgto <95735342+venomgto@users.noreply.github.com>
2024-04-16 15:58:21 -0700

    Theater Reservations

--- workbook-2/demos/TheaterReservations/src/main/java/com/pluralsight/Main.java
+++ workbook-2/demos/TheaterReservations/src/main/java/com/pluralsight/Main.java
@@ -14,10 +14,22 @@ public class Main
         System.out.print("Please enter your name: ");
         String fullName = userInput.nextLine();
 
-        System.out.print("What date will you be coming (MM/DD/YYYY)?");
+        System.out.print("What date will you be coming (MM/dd/yyyy)?");
         String date = userInput.nextLine();
+
+        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("MM/dd/yyyy");
+        LocalDate ticketDay = LocalDate.parse(date, dateFormatter);
 
         System.out.print("How many tickets would you like? ");
         String tickets = userInput.nextLine();
+        int ticketsCount = Integer.parseInt(tickets);
+
+        if(ticketsCount > 1)
+        {
+            System.out.println(ticketsCount + " tickets reserved for " + ticketDay + " under " + fullName);
+        }
+        else {
+            System.out.println(ticketsCount + " ticket reserved for " + ticketDay + " under " + fullName);
+        }
     }
 }
