a5494e1bca95f13c605fe0c08bc742b66401d41f
venomgto <95735342+venomgto@users.noreply.github.com>
2024-04-18 13:12:09 -0700

    completed looping excercise

--- workbook-2/demos/LoopingExcercise/src/main/java/com/pluralsight/Main.java
+++ workbook-2/demos/LoopingExcercise/src/main/java/com/pluralsight/Main.java
@@ -1,11 +1,48 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class Main
 {
-    public static void main(String[] args)
+    static Scanner userInput = new Scanner(System.in);
+
+    public static void main(String[] args)throws InterruptedException
     {
+        whileLoop();
+        doWhileLoop();
+        forLoop();
+    }
+
+    public static void whileLoop()
+    {
+        int count = 0;
+
+        while(count < 10)
+        {
+            System.out.println("I love Java");
+            count++;
+        }
 
     }
 
-    public
+    public static void doWhileLoop()
+    {
+        int count = 0;
+
+        do
+        {
+            System.out.println("I love Java!");
+            count++;
+        } while(count < 5);
+    }
+
+    public static void forLoop() throws InterruptedException {
+        for(int i = 10; i >= 1; i--)
+        {
+            System.out.println(i);
+
+            Thread.sleep(1000);
+        }
+        System.out.println("Launch!");
+    }
 }
