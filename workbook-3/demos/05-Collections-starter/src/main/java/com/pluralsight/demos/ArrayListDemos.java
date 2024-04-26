d9565e810c9effd92866a117e5e52cae942837e2
Gregor Dzierzon <gregor@fullstackdevs.net>
2024-04-26 10:50:22 -0600

    demo code for Friday

---
+++ workbook-3/demos/05-Collections-starter/src/main/java/com/pluralsight/demos/ArrayListDemos.java
@@ -0,0 +1,32 @@
+package com.pluralsight.demos;
+
+import java.util.ArrayList;
+
+public class ArrayListDemos
+{
+    public static void main(String[] args)
+    {
+        ArrayList<String> heroes = new ArrayList<String>();
+        heroes.add("Spider-Man");
+        heroes.add("Iron Man");
+        heroes.add("Thor");
+        heroes.add("Captain America");
+        heroes.add("Hulk");
+        heroes.add("Black Widow");
+        heroes.add("Hawkeye");
+        heroes.add("Ant-Man");
+        heroes.add("Wasp");
+        heroes.add("Black Panther");
+        heroes.add("Doctor Strange");
+        heroes.add("Scarlet Witch");
+        heroes.add("Vision");
+        heroes.add("Falcon");
+        heroes.add("Winter Soldier");
+        heroes.add("Star-Lord");
+        heroes.add("Gamora");
+        heroes.add("Drax the Destroyer");
+        heroes.add("Rocket Raccoon");
+        heroes.add("Groot");
+    }
+
+}
---
+++ workbook-3/demos/05-Collections-starter/src/main/java/com/pluralsight/demos/HashMapDemos.java
@@ -0,0 +1,5 @@
+package com.pluralsight.demos;
+
+public class HashMapDemos
+{
+}
