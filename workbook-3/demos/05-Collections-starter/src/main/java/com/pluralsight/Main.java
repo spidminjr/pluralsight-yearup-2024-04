2a8fc75af7c1feecdffc69862f884f2e24fa00d6
Gregor Dzierzon <gregor@fullstackdevs.net>
2024-04-26 10:54:05 -0600

    update demos

--- workbook-3/demos/05-Collections-starter/src/main/java/com/pluralsight/Main.java
+++ workbook-3/demos/05-Collections-starter/src/main/java/com/pluralsight/Main.java
@@ -6,17 +6,6 @@ public class Main
 {
     public static void main(String[] args)
     {
-        Logger logger = new Logger("demo");
-        logger.logMessage("info", "application start");
 
-
-        var logEntries = logger.readLog();
-        for(var logEntry : logEntries)
-        {
-            System.out.println(logEntry.getMessage());
-        }
-
-
-        logger.logMessage("info","application end");
     }
 }
