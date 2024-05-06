ba048616c17cffeb70d2fe976f6a0803dc76637d
Gregor Dzierzon <gregor@fullstackdevs.net>
2024-05-06 15:10:38 -0600

    demo code

--- capstone-01-demo/src/main/java/com/pluralsight/ui/UserInterface.java
+++ capstone-01-demo/src/main/java/com/pluralsight/ui/UserInterface.java
@@ -4,11 +4,14 @@ import com.pluralsight.models.Transaction;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 // this is the VIEW of the MVC pattern
 // all user input and output logic should be here
 public class UserInterface
 {
+    private Scanner userInput = new Scanner(System.in);
+
     public int getHomeScreenSelection()
     {
         return 0;
