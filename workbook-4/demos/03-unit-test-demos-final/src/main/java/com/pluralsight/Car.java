033c5fbd01e75a9535886ecfc68063eee237a7db
Gregor Dzierzon <gregor@fullstackdevs.net>
2024-05-08 14:25:46 -0600

    demo final for unit tests

---
+++ workbook-4/demos/03-unit-test-demos-final/src/main/java/com/pluralsight/Car.java
@@ -0,0 +1,43 @@
+package com.pluralsight;
+
+public class Car
+{
+    private String make;
+    private String model;
+    private int speed;
+
+    public Car(String make, String model)
+    {
+        this.make = make;
+        this.model = model;
+        this.speed = 0;
+    }
+
+    public String getMake()
+    {
+        return make;
+    }
+
+    public String getModel()
+    {
+        return model;
+    }
+
+    public int getSpeed()
+    {
+        return speed;
+    }
+
+    public void accelerate(int increaseBy)
+    {
+        if(increaseBy > 0)
+        {
+            speed += increaseBy;
+        }
+    }
+
+    public void brake(int decreaseBy)
+    {
+        speed -= decreaseBy;
+    }
+}
