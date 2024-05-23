09b7f4816c92884c21d3bcdf6467e1f3af55a5f0
Gregor Dzierzon <gregor@fullstackdevs.net>
2024-05-23 09:19:39 -0600

    updated starter code

--- workbook-6/streams-part-1/src/main/java/com/pluralsight/applicaton/PeopleApp.java
+++ workbook-6/streams-part-1/src/main/java/com/pluralsight/applicaton/PeopleApp.java
@@ -3,16 +3,16 @@ package com.pluralsight.applicaton;
 import com.pluralsight.models.Employee;
 import com.pluralsight.models.Person;
 import com.pluralsight.services.PeopleBuilder;
+import com.pluralsight.services.PersonService;
 import com.pluralsight.ui.UserInterface;
 import com.pluralsight.ui.enumerations.UserChoice;
 
-import java.sql.Array;
-import java.util.ArrayList;
 import java.util.List;
 
 public class PeopleApp
 {
     private List<Person> people;
+    private final PersonService service = new PersonService();
 
     public void run()
     {
@@ -54,15 +54,8 @@ public class PeopleApp
     {
         // prompt user for first name and display all people with that name
         String name = UserInterface.getSearchNameFromUser();
-        List<Person> searchResults = findPeople(people, name);
+        List<Person> searchResults = service.findPeople(people, name);
         UserInterface.displayPeople(searchResults, "Search people by name: " + name);
-    }
-
-    private List<Person> findPeople(List<Person> people, String name)
-    {
-        // search for people by first name
-        // return a new List<Person> with the search results
-        return new ArrayList<>();
     }
     /* ********************************************
     END Search by name
@@ -77,13 +70,8 @@ public class PeopleApp
     {
         // prompt user for an age and display all people with that age
         int age = UserInterface.getSearchAgeFromUser();
-        List<Person> searchResults = findPeople(people, age);
+        List<Person> searchResults = service.findPeople(people, age);
         UserInterface.displayPeople(searchResults, "Search people by age: " + age);
-    }
-
-    private List<Person> findPeople(List<Person> people, int age)
-    {
-        return new ArrayList<>();
     }
     /* ********************************************
     END Search by age
@@ -97,13 +85,8 @@ public class PeopleApp
     private void displayAverageAge()
     {
         // calculate and display the average age of all people in the list
-        int averageAge = calculateAverageAge(people);
+        int averageAge = service.calculateAverageAge(people);
         UserInterface.displayMessage("The average age of all people is: " + averageAge);
-    }
-
-    private int calculateAverageAge(List<Person> people)
-    {
-        return 0;
     }
     /* ********************************************
     END Display average age
@@ -118,13 +101,8 @@ public class PeopleApp
     {
         // find and display the age of the oldest person in the list
         // don't display the person's info, just their age
-        int oldest = findOldestAge(people);
-        UserInterface.displayMessage(STR."The oldest person is: \{oldest} years old");
-    }
-
-    private int findOldestAge(List<Person> people)
-    {
-        return 0;
+        int oldest = service.findOldestAge(people);
+        UserInterface.displayMessage("The oldest person is: " + oldest + " years old");
     }
     /* ********************************************
     END Display oldest age
@@ -139,79 +117,28 @@ public class PeopleApp
     {
         // find and display the age of the youngest person in the list
         // don't display the person's info, just their age
-        int youngest = findYoungestAge(people);
-        UserInterface.displayMessage(STR."The youngest person is: \{youngest} years old");
+        int youngest = service.findYoungestAge(people);
+        UserInterface.displayMessage("The youngest person is: " + youngest + " years old");
     }
 
-    private int findYoungestAge(List<Person> people)
-    {
-        return 0;
-    }
-    /* ********************************************
-    BEGIN Display youngest age
-    ********************************************* */
-
-
-
-    /* ********************************************
-    BEGIN Sort youngest first
-    ********************************************* */
     private void displaySortedYoungestFirst()
     {
         // display all info for all people, sorted by age, youngest first
-        List<Person> sortedList = sortYoungestFirst(people);
+        List<Person> sortedList = service.sortYoungestFirst(people);
         UserInterface.displayPeople(sortedList, "People sorted by age - youngest first");
     }
 
-    private List<Person> sortYoungestFirst(List<Person> people)
-    {
-        return new ArrayList<>();
-    }
-    /* ********************************************
-    END Sort youngest first
-    ********************************************* */
-
-
-
-    /* ********************************************
-    BEGIN Sort oldest first
-    ********************************************* */
     private void displaySortedOldestFirst()
     {
         // display all info for all people, sorted by age, youngest first
-        List<Person> sortedList = sortOldestFirst(people);
+        List<Person> sortedList = service.sortOldestFirst(people);
         UserInterface.displayPeople(sortedList, "People sorted by age - oldest first");
     }
 
-    private List<Person> sortOldestFirst(List<Person> people)
-    {
-        return new ArrayList<>();
-    }
-    /* ********************************************
-    END Sort oldest first
-    ********************************************* */
-
-
-
-    /* ********************************************
-    BEGIN Create employees
-    ********************************************* */
     private void displayEmployees()
     {
         // display all people as employees
-        List<Employee> employees = createEmployees(people);
+        List<Employee> employees = service.createEmployees(people);
         UserInterface.displayEmployees(employees);
     }
-
-    private List<Employee> createEmployees(List<Person> people)
-    {
-        // populate the list of Employees with...
-        // create a new Employee for each person in your people list
-        // the salary of each employee is based on their age $3,000 per year
-        // i.e. a 10 year old = $30,000, a 40 year old = $120,000 etc
-        return new ArrayList<>();
-    }
-    /* ********************************************
-    END Create employees
-    ********************************************* */
 }
